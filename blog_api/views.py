82e107cb348b6c948017a187acd4c2546536d723
Sjoerd Vink <sjoerdvink@icloud.com>
2021-03-23 12:06:21 +0100

    initial commit

---
+++ blog_api/views.py
@@ -0,0 +1,9 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+class PostList(generics.ListCreateAPIView):
+    pass
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    pass
